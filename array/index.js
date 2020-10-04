c055169c1f7cbab1c265b5b3fef8ef14b9751afd
Sneha Nakarmi <nakarmisneha@gmail.com>
2020-10-04 18:47:45 +0545

    array and datatypes added.

---
+++ array/index.js
@@ -0,0 +1,18 @@
+//let fruit1 = 'apple';
+//let fruit2 = 'mango';
+//let fruit3 = 'litchi';
+
+//console.log(fruit1, fruit2, fruit3);
+
+//let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+//console.log(newArr[2][2]);
+
+//let dc = ['superman', 'batman', ['flash','wonderwoman'], 'aquaman'];
+//dc.splice(2,0,'joker');
+//console.log(dc);
+//dc[2].unshift('joker'); //dc[2].splice(0,0,'joker')
+//console.log(dc);
+
+let dc = ['superman', 'batman', 'aquaman'];
+dc.splice(3,0,'joker');
+console.log(dc);
---
+++ data_types/index.js
@@ -0,0 +1,16 @@
+//let variable_name_second, another_variable;
+//console.log("==========");
+//console.log(variable_name_second);
+//console.log(another_variable);
+
+let integer_value=1;
+let string_value='hello world';
+let bool_value=false;
+let undefined_value;
+
+console.log("=======");
+console.log(integer_value);
+console.log(string_value);
+console.log(bool_value);
+console.log(undefined_value);
+console.log("======");
