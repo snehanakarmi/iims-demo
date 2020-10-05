c7db006c0544903d760315953c93c0f8ce6fdc03
Sneha Nakarmi <nakarmisneha@gmail.com>
2020-10-05 15:59:42 +0545

    functions added.

--- array/index.js
+++ array/index.js
@@ -13,6 +13,41 @@
 //dc[2].unshift('joker'); //dc[2].splice(0,0,'joker')
 //console.log(dc);
 
+//let dc = ['superman', 'batman', 'aquaman'];
+//dc.splice(3,0,'joker');//dc[3]='joker'; dc.push('joker')
+//console.log(dc);
+
+//concat
 let dc = ['superman', 'batman', 'aquaman'];
-dc.splice(3,0,'joker');
-console.log(dc);
+let villain=['joker'];
+let dcHero=['flash'];
+//console.log(dc.concat(villain,dcHero));
+
+//slice
+//let favFood = ['Laphing', 'Pizza', 'Mo:Mo', 'pasta', 'ramen'];
+//let food=favFood.slice(0);
+//console.log(favFood);
+
+//slice with 2 indexes
+//let fruits = ['mango', 'banana', 'apple', 'pears'];
+//let citrus = fruits.slice(1,2);
+//console.log(fruits);
+//console.log(citrus);
+
+let myself = {};
+myself.name = 'Sneha Nakarmi';
+myself.address = 'Kirtipur';
+myself.college = 'IIMS';
+myself.faculty = 'IT';
+myself.id = 1001953478;
+//console.log(myself);
+
+//let person= {firstName: "Sneha", lastName: "Nakarmi", age: 20, address: "Kirtipur"};
+//console.log('My name is ' +myself.name+ 'I live in ' +myself.address+ 'I am studying ' +myself.faculty+ ' in' +myself.college+ ' college. And my student id is: '+myself.id+ '.');
+
+//function
+function myIntro(firstName, lastName){
+	return firstName + lastName;
+}
+let result = myIntro('Sneha', 'Nakarmi');
+console.log(result);
